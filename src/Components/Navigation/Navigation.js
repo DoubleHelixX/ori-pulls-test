0de217a5cc2779857705bd42c1024f74af8aa516
oriofir <97814056+oriofir@users.noreply.github.com>
2022-05-27 16:50:16 -0700

    styling nav bar css

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Navigation.css";
 
 function Navigation(props) {
   return (
     <>
-      <h1>PULLS</h1>
+      <Link className="nav" to="/">
+        <h1 className="title">PULLS</h1>
+      </Link>
+      <Link className="nav" to="/">
+        Dashboard
+      </Link>
+      <Link className="nav" to={"/projects"}>
+        Projects
+      </Link>
+      <Link className="nav" to={"/forum"}>
+        Forum
+      </Link>
+      <Link className="nav" to={"/database"}>
+        Database
+      </Link>
+      <Link className="nav" to={"/search"}>
+        Search
+      </Link>
     </>
   );
 }
