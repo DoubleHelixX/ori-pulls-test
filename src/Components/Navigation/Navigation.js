aff9c592886af592d65ddd4ea2ad5b05dd17079d
oriofir <97814056+oriofir@users.noreply.github.com>
2022-05-31 15:57:03 -0700

    forum positioning

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar, Image } from "react-bootstrap";
+
 import "./Navigation.css";
 
 function Navigation(props) {
@@ -31,6 +32,17 @@ function Navigation(props) {
             <Link className="nav" to={"/search"}>
               Search
             </Link>
+            <Image
+              src={"./D8717CE0-50AA-4887-9AF9-3EE9CD68D219_1_105_c.jpeg"}
+              title="source: imgur.com"
+              className="img-fluid shadow-4"
+              style={{
+                borderRadius: "50%",
+                width: 50,
+                display: "flex",
+                flexDirection: "column",
+              }}
+            />
           </Nav>
         </Navbar.Collapse>
       </Navbar>
